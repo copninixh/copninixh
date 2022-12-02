40f0161e7767912fa8eeb6741ad711f18f0d3618
copninich <devkampanart.ch@gmail.com>
2022-12-02 23:55:47 +0700

    fixed _app.js error

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,8 @@
 import '../styles/globals.css'
 import '../styles/index.css'
 import {Component} from 'react'
+import type { AppProps } from 'next/app'
 
-export default function MyApp({ Component, pageProps }) {
+export default function MyApp({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />
 }
